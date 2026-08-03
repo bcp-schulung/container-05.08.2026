fb84e674e74df89e7b5e4db0e64f22674b8940a5
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-08-03 11:13:19 +0200

    Add additional commands for managing nginx container lifecycle

--- exercises/exercise-1/exercise.sh
+++ exercises/exercise-1/exercise.sh
@@ -2,3 +2,16 @@ sudo docker run -d -p 3000:80 nginx
 
 sudo docker ps -a
 sudo docker logs $(sudo docker ps -q -l)
+
+sudo docker ps
+sudo docker exec -it 38d3fa5d51b2 /bin/bash
+
+apt update
+apt install -y curl nano
+nano /usr/share/nginx/html/index.html
+
+exit
+
+sudo docker stop 38d3fa5d51b2
+
+sudo docker rm 38d3fa5d51b2
